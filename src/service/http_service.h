75df91504610fcaf6680e3ddbf795969f9078bb1
123youyouer <179261275@qq.com>
2018-08-15 18:10:13 +0800

    2018-08-15

--- src/service/http_service.h
+++ src/service/http_service.h
@@ -4,11 +4,33 @@
 
 #ifndef BOXED_HTTP_SERVICE_H
 #define BOXED_HTTP_SERVICE_H
-
+#include "core/distributed.hh"
+#include <boost/hana.hpp>
 
 class http_service {
-
+    const unsigned int _port;
+    seastar::future<const unsigned int> uu(){return seastar::make_ready_future<const unsigned int>(_port);}
+public:
+    http_service(unsigned int port):_port(port){}
+    void run(){
+    }
+    seastar::future<> stop(){return seastar::make_ready_future();}
 };
 
+extern seastar::distributed<http_service> _g_http_service;
 
+template<typename F>
+auto listen_proc=[](uint16_t _port){
+    return seastar::do_with(seastar::engine().listen(seastar::make_ipv4_address({_port}),{true}),
+                     [](auto& listener){
+                         return seastar::keep_doing([&listener]{
+                             return listener.accept().then([](seastar::connected_socket fd, seastar::socket_address addr)mutable{
+                                 return seastar::smp::submit_to(seastar::engine().cpu_id(),[fd=std::move(fd)]{
+                                     return F()(fd);
+                                 });
+                             });
+                         });
+                     }
+    );
+};
 #endif //BOXED_HTTP_SERVICE_H
