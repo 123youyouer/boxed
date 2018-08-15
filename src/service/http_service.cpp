71d2369ca26904f3612e7ccca69720f8a074a283
123youyouer <179261275@qq.com>
2018-08-15 12:00:45 +0800

    初始化提交

---
+++ src/service/http_service.cpp
@@ -0,0 +1,5 @@
+//
+// Created by null on 18-8-15.
+//
+
+#include "http_service.h"
---
+++ src/service/http_service.h
@@ -0,0 +1,14 @@
+//
+// Created by null on 18-8-15.
+//
+
+#ifndef BOXED_HTTP_SERVICE_H
+#define BOXED_HTTP_SERVICE_H
+
+
+class http_service {
+
+};
+
+
+#endif //BOXED_HTTP_SERVICE_H
