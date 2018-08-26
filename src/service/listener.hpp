4f410a9fbd98508ef5da96793d93707b4555e959
123youyouer <179261275@qq.com>
2018-08-26 23:45:58 +0800

    20180826

--- src/service/listener.hpp
+++ src/service/listener.hpp
@@ -11,22 +11,6 @@
 
 template<typename CONN_PROC>
 constexpr
-auto listen_proc=[](uint16_t _port){
-    return [&_port](CONN_PROC&& _proc){
-        return seastar::do_with(seastar::engine().listen(seastar::make_ipv4_address({_port}),{true}),
-                                [&_proc](auto& listener){
-                                    return seastar::keep_doing([&listener,&_proc]{
-                                        std::cout<<"keep_doing"<<std::endl;
-                                        return listener.accept().then([&_proc](seastar::connected_socket fd, seastar::socket_address addr)mutable{
-                                            _proc(fd,addr);
-                                        });
-                                    });
-                                }
-        );
-    };
-};
-template<typename CONN_PROC>
-constexpr
 auto build_listen_proc(CONN_PROC&& _proc){
     return [&_proc](uint16_t _port){
         return seastar::do_with(seastar::engine().listen(seastar::make_ipv4_address({_port}),{true}),
